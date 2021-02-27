40cf6202a5536a5a06c538092b84001ca054234f
Martin Lim <trabin9@gmail.com>
2021-02-26 16:12:26 -0800

    debug

---
+++ app.spec
@@ -0,0 +1,37 @@
+# -*- mode: python ; coding: utf-8 -*-
+
+block_cipher = None
+
+
+a = Analysis(['src\\app.py'],
+             pathex=['C:\\Users\\lim_m\\ownCloud\\ATE\\AA_Code\\flask_demo'],
+             binaries=[],
+             datas=[],
+             hiddenimports=[],
+             hookspath=[],
+             runtime_hooks=[],
+             excludes=[],
+             win_no_prefer_redirects=False,
+             win_private_assemblies=False,
+             cipher=block_cipher,
+             noarchive=False)
+pyz = PYZ(a.pure, a.zipped_data,
+             cipher=block_cipher)
+exe = EXE(pyz,
+          a.scripts,
+          [],
+          exclude_binaries=True,
+          name='app',
+          debug=False,
+          bootloader_ignore_signals=False,
+          strip=False,
+          upx=True,
+          console=False , version='fixture\\version.txt', icon='fixture\\rns.ico')
+coll = COLLECT(exe,
+               a.binaries,
+               a.zipfiles,
+               a.datas,
+               strip=False,
+               upx=True,
+               upx_exclude=[],
+               name='app')
---
+++ test/test.py
@@ -0,0 +1,36 @@
+from app import app
+import unittest
+
+class flask_app_demo(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        pass
+
+    @classmethod
+    def tearDownClass(cls):
+        pass
+
+    def setUp(self):
+        self.app = app.test_client()                # creates a test client
+        self.app.testing = True                     # propagate the exceptions to the test client
+
+    def tearDown(self):
+        pass
+
+###############################################################################
+### <Test>
+###############################################################################
+    def test_home_status_code(self):
+        result = self.app.get('/')                  # sends HTTP GET to application
+        self.assertEqual(result.status_code, 200)   # assert the status code of the response
+
+    def test_home_data(self):
+        result = self.app.get('/')                  # sends HTTP GET to application
+        self.assertEqual(result.data, "Hello World!!!")          # assert the response data
+
+###############################################################################
+### </Test>
+###############################################################################
+if __name__ == '__main__':                                  # pragma: no cover
+    suite = unittest.TestLoader().loadTestsFromTestCase(flask_app_demo)
+    unittest.TextTestRunner(verbosity=2).run(suite)
